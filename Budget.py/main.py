afbce49f669216752bfe7d69951da9976a217e97
Shiva itani <itanishiva2015@gmail.com>
2023-07-26 20:27:03 -0400

    Project has started

---
+++ Budget.py/main.py
@@ -0,0 +1,52 @@
+class Category:
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
+
+    def withdraw(self, amount, description=""):
+        if self.check_funds(amount):
+            self.ledger.append({"amount": -amount, "description": description})
+            return True
+        return False
+
+    def get_balance(self):
+        balance = 0
+        for item in self.ledger:
+            balance += item["amount"]
+        return balance
+
+    def transfer(self, amount, category):
+        if self.check_funds(amount):
+            self.withdraw(amount, f"Transfer to {category.name}")
+            category.deposit(amount, f"Transfer from {self.name}")
+            return True
+        return False
+
+    def check_funds(self, amount):
+        if amount > self.get_balance():
+            return False
+        return True
+
+    def __str__(self):
+        title = f"{self.name:*^30}\n"
+        items = ""
+        for item in self.ledger:
+            items += f"{item['description'][:23]:23}{item['amount']:>7.2f}\n"
+        total = f"Total: {self.get_balance():.2f}"
+        return title + items + total
+food = Category("Food")
+clothing = Category("Clothing")
+
+food.deposit(1000, "Initial deposit")
+clothing.deposit(500, "Initial deposit")
+
+food.withdraw(10.15, "Groceries")
+clothing.withdraw(50, "Shirt")
+
+food.transfer(50, clothing)
+
+print(food)
+print(clothing)
